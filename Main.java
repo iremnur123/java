928afa38c7bcf587c1f41cbc97b82e0ce5ae86cd
iremnur123 <hasturkiremnur9@gmail.com>
2023-11-02 20:28:03 +0300

    java

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+// Online IDE - Code Edıtor,Compiler,Interpreter
+
+public class Main
+{
+    public static void main(String[] args) {
+        int a=18;   //rakam vb. şeylerde int kullanılır.
+            String ad="irem nur";
+            String soyad="hastürk";
+
+            System.out.println("merhaba");
+            System.out.println(a);
+            System.out.println(ad);
+            System.out.println(soyad);
+    }
+}
